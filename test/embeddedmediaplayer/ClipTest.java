f92a450f6e25666e937180719da3d6c85318e4c7
mo18abp <mo18abp@ENT-D410-16.herts.ac.uk>
2019-04-08 13:21:02 +0100

    Added testSetEndBeforeStartKeepsPreviousValue

--- test/embeddedmediaplayer/ClipTest.java
+++ test/embeddedmediaplayer/ClipTest.java
@@ -46,10 +46,13 @@ public class ClipTest {
         System.out.println("testSetTitleToEmptyStringKeepsPreviousValue");
         Clip instance = new Clip();
         String InitialTitle = "Original Title";
+        
         instance.setTitle(InitialTitle); // try to set Empty Title
+        
         String EmptyTitle = " ";
         instance.setTitle(EmptyTitle);              // try to set Empty Title
         String ModifiedTitle = instance.getTitle(); // check Empty Title is set or not 
+        
         assertTrue(InitialTitle.equals(ModifiedTitle));// compare original title and resulted title
         System.out.println("Empty Title not set");
     }
@@ -57,6 +60,21 @@ public class ClipTest {
     @Test
     public void testSetEndBeforeStartKeepsPreviousValue()
     {
+       System.out.println("testSetEndBeforeStartKeepsPreviousValue()");
+       Clip instance = new Clip();
+       String InitialTitle = "Sub video"; //Create sub video clip
+       instance.setTitle(InitialTitle);   // Set Ttile  
+       
+       int OriginalStartTime = 10; // sub video start time
+       int originalEndtime = 50;// sub video end time
+       instance.setMax(100);//set master video to 100 seconds video
+       instance.setStart(OriginalStartTime);//Set start time to sub video from 10th sec 
+       instance.setEnd(originalEndtime);// set end time to sub video at 50th second
+       int EndTime = instance.getEnd(); //Get endtime of the video
+       instance.setStart(EndTime); 
+       int CurrentStartTime = instance.getStart();//chek start time was set to endtime, usually it wont allow
+       assertEquals(OriginalStartTime,CurrentStartTime);
+       System.out.println("Start time is same as previous value");
     }
 
     @Test
